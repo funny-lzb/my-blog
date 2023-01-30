c1a58098177ab5a8d1a07bacd5abeec36468d987
unknown <lzb129239@gmail.com>
2023-01-30 23:24:47 +0800

    msg

---
+++ server/server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const lyricsFinder = require('lyrics-finder')
+const SpotifyWebApi = require('spotify-web-api-node')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:5173/hobby',
+    clientId: '0a6a5af0d4e9497f8496e74c54c9503a',
+    clientSecret: '1d6e39691bb34b76857b9e6bb43d4e4c',
+    refreshToken
+  })
+
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+app.post('/login', (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:5173/hobby',
+    clientId: '0a6a5af0d4e9497f8496e74c54c9503a',
+    clientSecret: '1d6e39691bb34b76857b9e6bb43d4e4c'
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in
+      })
+    })
+    .catch(err => {
+      res.sendStatus(400)
+    })
+})
+
+app.get('/lyrics', async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) || '找不到歌词'
+  res.json({ lyrics })
+
+  console.log(lyrics)
+})
+app.listen(5174)
