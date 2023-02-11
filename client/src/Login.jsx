38025dd407d06d427d885a23ad7f2df62c252086
unknown <lzb129239@gmail.com>
2023-02-11 21:58:10 +0800

    last commit

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button"
 import { Container } from "react-bootstrap"
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=0a6a5af0d4e9497f8496e74c54c9503a&response_type=code&redirect_uri=https://lzb-blog.netlify.app/hobby&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  "https://accounts.spotify.com/authorize?client_id=0a6a5af0d4e9497f8496e74c54c9503a&response_type=code&redirect_uri=http://localhost:5173/hobby&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
   const [show, setShow] = useState(false)
