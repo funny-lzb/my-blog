d84972798db1cea8560cc6bc6f6432e32817d229
unknown <lzb129239@gmail.com>
2023-03-28 21:46:08 +0800

    remove state

--- client/src/hooks/UseFadeInRight.jsx
+++ client/src/hooks/UseFadeInRight.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, {  useEffect, useRef } from 'react'
 import '../css/fadeIn.css'
 
 export default function UseFadeInRight({ targetPosition, children }) {
